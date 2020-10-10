26fd95891a76d71ca057a60d72502cd65d7e633b
Galen Guyer <galen@galenguyer.com>
2020-10-09 21:15:51 -0400

    Add pylint and HelloWorld

---
+++ README.md
@@ -0,0 +1,4 @@
+# Minecraft Manager
+A dumb tool to create minecraft servers on linux the way I want
+
+#### PyLint
---
+++ mcm/__init__.py
@@ -0,0 +1,13 @@
+"""
+option parsing file for the minecraft manager
+"""
+
+def main():
+    """
+    main method for parsing arguments
+    """
+    print('Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
