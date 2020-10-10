64002e4b6fa403d4a4e6835dd35a607cf75c0212
Galen Guyer <galen@galenguyer.com>
2020-10-09 22:42:57 -0400

    pylint cleanups

--- mcm/__init__.py
+++ mcm/__init__.py
@@ -16,15 +16,15 @@ def reporthook(count, block_size, total_size):
     """
     used for printing the status of a download
     """
-    global start_time
+    global START_TIME # pylint: disable=global-variable-undefined
     if count == 0:
-        start_time = time.time()
+        START_TIME = time.time()
         return
-    duration = time.time() - start_time
+    duration = time.time() - START_TIME
     progress_size = int(count * block_size)
     speed = int(progress_size / (1024 * duration))
     percent = min(int(count*block_size*100/total_size),100)
-    sys.stdout.write("\r%d%%, %d MB, %d KB/s, %d seconds passed" %
+    sys.stdout.write('\r%d%%, %d MB, %d KB/s, %d seconds passed' %
                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
 
@@ -43,7 +43,9 @@ def check_root() -> None:
     if get_is_root():
         pass
     else:
-        print('You are not running this script as root. This means dependencies cannot be automatically installed, and a service file cannot be created.')
+        print('You are not running this script as root. ' + \
+            'This means dependencies cannot be automatically installed, ' + \
+            'and a service file cannot be created.')
         carryon = input('Are you sure you wish to continue? [yN]: ')
         if carryon.lower() == 'y':
             pass
@@ -51,39 +53,42 @@ def check_root() -> None:
             sys.exit('Please run the script as root to continue')
 
 
-class minecraft():
-    def __init__(self):
-        """
-        repl loop for vanilla minecraft
-        """
-        versions = json.loads(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json').read().decode('utf-8'))
-        uver = input(f'What version of Minecraft would you like? The default is the lateset release ({versions["latest"]["release"]}): ')
-        if uver:
-            uver = uver.lower()
-        else:
-            uver = versions['latest']['release']
+def minecraft():
+    """
+    repl loop for vanilla minecraft
+    """
+    versions = json.loads(
+        urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json')\
+            .read().decode('utf-8'))
+    uver = input('What version of Minecraft would you like? ' + \
+        'The default is the lateset release ({versions["latest"]["release"]}): ')
+    if uver:
+        uver = uver.lower()
+    else:
+        uver = versions['latest']['release']
 
-        server_url = None
-        for version in versions['versions']:
-            if version['id'] == uver:
-                version_json = json.loads(urlopen(version['url']).read().decode('utf-8'))
-                server_url = version_json['downloads']['server']['url']
-                break
-        if server_url is None:
-            print(f'Invalid version {uver}. Exiting')
-            sys.exit(1)
-            
-        path = input(f"Where would you like file saved? The default is the current directory ({os.getcwd()})")
-        if not path:
-            path = os.getcwd()
-        if path[-1] != '/':
-            path += '/'
-        if os.path.exists(path) or os.access(os.path.dirname(path), os.W_OK):
-            urlretrieve(server_url, f"{path}minecraft-server-{uver}.jar", reporthook)
-            time.sleep(0.5)
-            print(f"\nDownloaded minecraft-server-{uver}.jar to {path}")
-        else:
-            sys.exit(f"'{path}' is not a valid or writeable path")
+    server_url = None
+    for version in versions['versions']:
+        if version['id'] == uver:
+            version_json = json.loads(urlopen(version['url']).read().decode('utf-8'))
+            server_url = version_json['downloads']['server']['url']
+            break
+    if server_url is None:
+        print(f'Invalid version {uver}. Exiting')
+        sys.exit(1)
+
+    path = input('Where would you like file saved? ' + \
+        f'The default is the current directory ({os.getcwd()})')
+    if not path:
+        path = os.getcwd()
+    if path[-1] != '/':
+        path += '/'
+    if os.path.exists(path) or os.access(os.path.dirname(path), os.W_OK):
+        urlretrieve(server_url, f'{path}minecraft-server-{uver}.jar', reporthook)
+        time.sleep(0.5)
+        print(f'\nDownloaded minecraft-server-{uver}.jar to {path}')
+    else:
+        sys.exit(f'"{path}" is not a valid or writeable path')
 
 
 def main():
