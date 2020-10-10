8f222aab6e01461f68d74c0edc3e6780976d9a59
Galen Guyer <galen@galenguyer.com>
2020-10-09 21:37:15 -0400

    Add argparser

--- mcm/__init__.py
+++ mcm/__init__.py
@@ -1,12 +1,16 @@
 """
 option parsing file for the minecraft manager
 """
+import argparse
+
 
 def main():
     """
     main method for parsing arguments
     """
-    print('Hello, World!')
+    parser = argparse.ArgumentParser(prog='mcm', \
+        description='minecraft server creator with start script and systemd service')
+    parser.parse_args()
 
 
 if __name__ == '__main__':
